948132d52c6d18a7e03bd70abb2855e3adb6daa5
pancho <pancho@localhost.localdomain>
2023-03-05 19:28:33 -0300

    nuke_util: update

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ from . import src as vina_scripter
 from .nuke_util import panels
 
 def setup():
-    panels.init(vina_scripter.scripter.scripter_widget, 'Vina Scripter')
+    panels.init('vina_scripter.vina_scripter.scripter.scripter_widget', 'Vina Scripter')
