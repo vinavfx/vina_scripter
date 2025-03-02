7c9ca5851b8e760029d0c9a613ea57335218859e
Francisco <fcocc77@gmail.com>
2025-03-01 22:36:37 -0300

    ...

--- __init__.py
+++ __init__.py
@@ -3,9 +3,11 @@
 # OFFICE --------> Senior VFX Compositor, Software Developer
 # WEBSITE -------> https://vinavfx.com
 # -----------------------------------------------------------
-import nuke
+import nuke  # type: ignore
+import os
 from . import src as vina_scripter
 from .nuke_util import panels
+from .nuke_util.nuke_util import get_nuke_path
 
 from .src.script_output import get_nuke_console
 
@@ -15,8 +17,10 @@ nuke_console_connected = False
 
 
 def setup():
-    panels.init(
-        'vina_scripter.vina_scripter.scripter_panel.panel_scripter', 'Vina Scripter')
+    path = os.path.join(get_nuke_path(), 'vina_scripter')
+    source = 'vina_scripter' if os.path.isdir(path) else 'vina.scripter'
+
+    panels.init( source + '.vina_scripter.scripter_panel.panel_scripter', 'Vina Scripter')
 
     panels.init_float_panel(
         vina_scripter.scripter_panel.float_scripter, 'vina_scripter')
